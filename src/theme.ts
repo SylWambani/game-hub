439b11146a0d9c7eae42e7d517d07ef4f9d26f3b
sylwambani <wambanisylvia2@gmail.com>
2025-07-31 05:58:01 +0300

    Implement dark mode

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+//import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import {
+  ColorModeButton,
+  DarkMode,
+  LightMode,
+  useColorMode,
+  useColorModeValue,
+} from "./components/ui/color-mode";
+
+/*const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
+
+const theme = extendTheme({ config });*/
+
+//export default theme;
