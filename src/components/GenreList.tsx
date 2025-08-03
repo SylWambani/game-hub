102923a1eaa720f95fc86e4d68e969b877d6a51f
sylwambani <wambanisylvia2@gmail.com>
2025-08-03 14:46:54 +0300

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,26 @@
-import useGenres from '../hooks/useGenres'
+import { HStack, Image, List, ListRoot, Text } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-      <ul>
-          {data.map(genre=><li key={genre.id}>{genre.name}</li>)}
-    </ul>
-  )
-}
+    <List.Root listStyle="none">
+      {data.map((genre) => (
+        <List.Item key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </List.Item>
+      ))}
+    </List.Root>
+  );
+};
 
-export default GenreList
+export default GenreList;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,12 @@
 import useData from "./useData";
 
-export interface Genre{
-    id: number;
-    name: string;
+export interface Genre {
+  id: number;
+  name: string;
+  image_background: string;
 }
 
-const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => useData<Genre>("/genres");
 
 //cord before refactoring in the useData hook which is a generic hook
 /*interface FetchGenresResponse{
